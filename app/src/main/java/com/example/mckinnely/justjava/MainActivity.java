c48597e0299fe53da53372319726fbad228aee17
McKinnely <McKinnely.bentley@gmail.com>
2016-11-01 22:16:03 -0500

    Android Mission Succeeding!

--- app/src/main/java/com/example/mckinnely/justjava/MainActivity.java
+++ app/src/main/java/com/example/mckinnely/justjava/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
@@ -27,8 +28,10 @@ public class MainActivity extends AppCompatActivity
      * This method is called when the order button is clicked.
      */
     int counter = 0;
-    public void submitOrder(View view) {
-        display(counter++);
+    public void submitOrder(View view)
+    {
+        display(2);
+        displayPrice(2 * 5);
     }
 
     /**
@@ -38,5 +41,14 @@ public class MainActivity extends AppCompatActivity
     {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void displayPrice(int number)
+    {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
